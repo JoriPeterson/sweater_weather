2411553057db2e7fdc36d1bd6fdb8309818f679f
Jori Peterson <joripeterson@gmail.com>
2019-09-15 16:07:55 -0600

    Set up route for retrieving forecast

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  GET "/api/v1/forecast?location=denver,co"
+  namespace :api do
+    namespace :v1 do
+      get "/forecast", to: 'weather#show'
+    end
+  end
 end
