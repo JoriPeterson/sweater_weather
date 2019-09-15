5f21cef97023d4acbd8fb98ccffb4e3d7bf6997f
Jori Peterson <joripeterson@gmail.com>
2019-09-15 16:10:42 -0600

    Draft forecast request spec

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -4,10 +4,9 @@ describe "Forcast API" do
   it "retrieves the forcast" do
     location = Location.create!(city: "denver", state: CO, lat: "...", lon: "...")
 
-    get '/api/v1/forecast?'
-    GET "/api/v1/forecast?location=#{location.city},#{location.state}"
+    get "api.darksky.net/forecast/#{ENV(DARKSKY_API_KEY)}/#{location.lat},#{location.lon}"
 
-    forcast = JSON.parse(response.body)["data"]
+    forcast = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
     expect(forcast.count).to eq(3)
