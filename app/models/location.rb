ba328b78de12882f4cb2b479077b63bc3ed6ad74
Jori Peterson <joripeterson@gmail.com>
2019-09-16 14:26:40 -0600

    Update models for location and forecast for refactor

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,6 @@ class Location
   attr_reader :id, :city, :state, :country, :lat, :lng
 
   def initialize(info)
-    @id = 1
     @city = info.first[:address_components][0][:long_name]
     @state = info.first[:address_components][2][:short_name]
     @country = info.first[:address_components][3][:long_name]
