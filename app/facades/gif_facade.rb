dec7bf748701dbcc01be3acc88a17c52341b00f2
Jori Peterson <joripeterson@gmail.com>
2019-09-16 10:13:39 -0600

    Work in gif facade

--- app/facades/gif_facade.rb
+++ app/facades/gif_facade.rb
@@ -1,17 +1,22 @@
 class GifFacade
 
   def initialize(weather_info)
-    @daily_summary = weather_info.daily
+    @weather_info = weather_info
+    @daily_summary = weather_info.daily.map { |day| day[:summary] }
   end
 
-
   def gifs
-    giphy_services.get_gifs
+    giphy_response = @daily_summary.each do |summary|
+      GiphyService.new(summary)
+    end
+    binding.pry
+    giphy_response
   end
 
   private
 
-  def giphy_services
-    @_giphy_services ||= GiphyService.new(@daily_summary)
-  end
+  # def giphy_services
+  #   GiphyService.new(@daily_summary)
+  #   # @_giphy_services ||= GiphyService.new(@daily_summary)
+  # end
 end
