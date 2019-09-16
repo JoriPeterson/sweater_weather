e2452666243fabed5336708560da81d02b995931
Jori Peterson <joripeterson@gmail.com>
2019-09-16 09:23:02 -0600

    Update weather controller

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::WeatherController < ApplicationController
 
   def show
     location_facade = LocationFacade.new(params[:location])
-    lat = location_facade.address_components.first.lat
-    lng = location_facade.address_components.first.lng
+    lat = location_facade.address_components.lat
+    lng = location_facade.address_components.lng
 
     forecast_facade = ForecastFacade.new(lat, lng)
     render json: ForecastSerializer.new(forecast_facade.weather_info)
