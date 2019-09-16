9b793e23fe027e5867ebd744bf2c2ccd045f824b
Jori Peterson <joripeterson@gmail.com>
2019-09-15 21:32:34 -0600

    Update service test for darksky

--- spec/services/darksky_service_spec.rb
+++ spec/services/darksky_service_spec.rb
@@ -8,16 +8,4 @@ describe DarkskyService do
   it "exists" do
 		expect(@darksky_service).to be_a(DarkskyService)
   end
-
-  # describe "instance methods" do
-  #   it "returns repositories", :vcr do
-  #     search = @darksky_service.repository_data
-  #     expect(search).to be_a Array
-  #     expect(search.count).to eq 30
-  #     expect(search[0]).to be_an Hash
-  #     member_data = search[0]
-  #
-  #     expect(member_data).to have_key :name
-  #   end
-  # end
 end
