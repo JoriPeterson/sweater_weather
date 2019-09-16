ab8b0c04c52d7675fd7c48b46f4981621e96648e
Jori Peterson <joripeterson@gmail.com>
2019-09-16 11:24:57 -0600

    Attach weather info to gif objects

--- app/facades/gif_facade.rb
+++ app/facades/gif_facade.rb
@@ -2,19 +2,21 @@ class GifFacade
 
   def initialize(weather_info)
     @weather_info = weather_info
+    @days = weather_info.daily
     @daily_summary = weather_info.daily.map { |day| day[:summary] }
   end
 
   def gifs
-    @daily_summary.each do |summary|
+    @daily_summary.map do |summary|
       GiphyService.new(summary).get_gifs
     end
   end
 
   def create_objects
-    binding.pry
     gifs[0][:data].map do |info|
-      Gif.new(info, @weather_info)
+      @days.map do |day|
+        Gif.new(info, day[:date], day[:summary])
+      end
     end
   end
 end
