03c40ff2ad21156a57e65a20319bc5fa4d665d87
Jori Peterson <joripeterson@gmail.com>
2019-09-15 21:33:01 -0600

    Refactor location poro

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,10 +3,10 @@ class Location
 
   def initialize(info)
     @id = 1
-    @city = info.first.last[0][:long_name]
-    @state = info.first.last[2][:short_name]
-    @country = info.first.last[3][:long_name]
-    @lat = info[:geometry][:location][:lat].round(4)
-    @lng = info[:geometry][:location][:lng].round(4)
+    @city = info.first[:address_components][0][:long_name]
+    @state = info.first[:address_components][2][:short_name]
+    @country = info.first[:address_components][3][:long_name]
+    @lat = info.first[:geometry][:location][:lat].round(4)
+    @lng = info.first[:geometry][:location][:lng].round(4)
   end
 end
