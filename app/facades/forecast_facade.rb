9cb8d66e4712ac7569e80a73b3ccd2bf21de0aee
Jori Peterson <joripeterson@gmail.com>
2019-09-15 21:18:46 -0600

    Add to forecast facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -4,4 +4,14 @@ class ForecastFacade
     @lat = lat
     @lng = lng
   end
+
+  def weather_info
+    Forecast.new(services.get_weather_details)
+  end
+
+  private
+
+  def services
+  @_services ||= DarkskyService.new(@lat, @lng)
+  end
 end
