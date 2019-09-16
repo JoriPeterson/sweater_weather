2e869a4e1c0526367ec44c0ef7170d098c811f7f
Jori Peterson <joripeterson@gmail.com>
2019-09-16 09:57:21 -0600

    Rethink gif facade

--- app/facades/gif_facade.rb
+++ app/facades/gif_facade.rb
@@ -1,25 +1,17 @@
 class GifFacade
 
   def initialize(weather_info)
-    binding.pry
-    @weather_info = weather_info.daily
-    daily_summary = weather_info.daily.map
+    @daily_summary = weather_info.daily
   end
 
-  def daily_summary
-    #array of daily summaries
-    @weather_info.map do |day|
-      day[:icon]
-    end
-  end
 
-  def gif_for_summary
-    services.get_gifs
+  def gifs
+    giphy_services.get_gifs
   end
 
   private
 
   def giphy_services
-    @_giphy_services ||= GiphyService.new(daily_summary)
+    @_giphy_services ||= GiphyService.new(@daily_summary)
   end
 end
