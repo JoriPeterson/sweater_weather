376b292e2707d7851431b2506538eaa0964ce411
Jori Peterson <joripeterson@gmail.com>
2019-09-16 09:39:11 -0600

    Add to gifs controller

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,6 +1,12 @@
-class GifsController < ApplicationController
+class Api::V1::GifsController < ApplicationController
 
   def show
+    location_facade = LocationFacade.new(params[:location])
+    lat = location_facade.address_components.lat
+    lng = location_facade.address_components.lng
+    forecast_facade = ForecastFacade.new(lat, lng)
+
+    GifFacade.new(forecast_facade.weather_info)
   end
-    
+
 end
