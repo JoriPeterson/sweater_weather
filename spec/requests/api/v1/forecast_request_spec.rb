bceca0a16a3a51985ff57c093d464ee6ba59c60e
Jori Peterson <joripeterson@gmail.com>
2019-09-15 19:00:04 -0600

    Update forecast request spec

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-describe "Forcast API" do
+describe "Forecast API" do
   it "retrieves the forcast" do
-    location = Location.create!(city: "denver", state: CO, lat: "...", lon: "...")
+    lat = 39.7392358
+    lng = -104.990251
 
-    get "api.darksky.net/forecast/#{ENV(DARKSKY_API_KEY)}/#{location.lat},#{location.lon}"
+    get "api.darksky.net/forecast/#{ENV(DARKSKY_API_KEY)}/#{lat},#{lng}"
 
     forcast = JSON.parse(response.body, symbolize_names: true)
 
