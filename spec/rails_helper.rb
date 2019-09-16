57c1b3391793396ff5082771705b54371979fdaf
Jori Peterson <joripeterson@gmail.com>
2019-09-15 18:59:20 -0600

    Revise rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,17 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'vcr'
-require 'webmock/rspec'
+# require 'webmock/rspec'
 
-VCR.configure do |config|
-  config.ignore_localhost = true
-  config.cassette_library_dir = 'spec/cassettes'
-  config.hook_into :webmock
-  config.configure_rspec_metadata!
-  config.filter_sensitive_data("<DARKSKY_API_KEY>") { ENV['DARKSKY_API_KEY'] }
-  config.filter_sensitive_data("<GEOCODE_API_KEY>") { ENV['GEOCODE_API_KEY'] }
-end
 # Add additional requires below this line. Rails is not loaded until this point!
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
