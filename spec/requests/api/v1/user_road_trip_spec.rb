9098fd18acdc818146e77c94f490e8f9ed90f08d
Jori Peterson <joripeterson@gmail.com>
2019-09-17 16:28:12 -0600

    Rename and update road trip request spec

--- spec/requests/api/v1/user_road_trip_spec.rb
+++ spec/requests/api/v1/user_road_trip_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 describe "User Roadtrip API" do
   it "takes an api key and returns future forecast info" do
-    stub_json("https://maps.googleapis.com/maps/api/geocode/json?address=denver,co&key=#{ENV["GEOCODE_API_KEY"]}", "./fixtures/geocode_denver.json")
-    # stub_json("https://maps.googleapis.com/maps/api/directions/json?origin=Denver,CO&destination=Pueblo,CO&key=#{ENV["GEOCODE_API_KEY"]}", "./fixtures/directions_den_to_pueblo.json")
     stub_json("https://maps.googleapis.com/maps/api/directions/json?destination=Pueblo,CO&key=AIzaSyCRySaQvXzqSfds4VdpR9R1pYLXgfIDFJ4&origin=", "./fixtures/directions_den_to_pueblo.json")
+    WebMock.allow_net_connect!
 
     user = User.create!(email: "whatever@example.com", password: "password", api_key: "d450965fb2f168d4ddc607")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -14,6 +13,9 @@ describe "User Roadtrip API" do
     info = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    expect(info[:forecast]).to eq("")
+
+    expect(info[:data][:attributes][:estimated_travel_time]).to eq("1 hour 46 mins")
+    expect(info[:data][:attributes][:temperature].class).to eq(Integer)
+    expect(info[:data][:attributes][:summary].class).to eq(String)
   end
 end
