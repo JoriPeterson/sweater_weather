327b7c3b8c5e6308f87350f51cd17fb3c1d8fade
Jori Peterson <joripeterson@gmail.com>
2019-09-16 19:58:45 -0600

    Write test for new user

---
+++ spec/requests/api/v1/new_user_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "New User API" do
+  it "creates a new user" do
+
+    post "/api/v1/users", params: { "email" => "whatever@example.com", "password" => "password", "password_confirmation" => "password" }.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+
+    user = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(user[:api_key].class).to eq(String)
+    expect(user[:api_key].length).to eq(22)
+  end
+end
