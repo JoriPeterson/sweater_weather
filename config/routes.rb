2e5a83d93a2fb2111be4961bb7310bb20a45ec42
Jori Peterson <joripeterson@gmail.com>
2019-09-16 21:55:47 -0600

    Add session route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get "/gifs", to: 'gifs#show'
       get "/backgrounds", to: 'backgrounds#show'
       post "users", to: 'users#create'
+      post "sessions", to: 'sessions#create'
     end
   end
 end
