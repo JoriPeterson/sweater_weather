cf9b3f010e448e736f7ef11be1380ee2c8d943d7
Jori Peterson <joripeterson@gmail.com>
2019-09-17 16:30:23 -0600

    Update road trip facade for darksky portion

--- app/facades/road_trip.rb
+++ app/facades/road_trip.rb
@@ -4,8 +4,22 @@ class RoadTrip
     @orgin = orgin
     @destination = destination
     @duration = trip_details.duration
-    @destination_lat = trip_details.destination_lat
-    @destination_lng = trip_details.destination_lng
+    @destination_lat = trip_details.destination_lat.round(4)
+    @destination_lng = trip_details.destination_lng.round(4)
+    @future_time = find_future_time
+  end
+
+  def find_future_time
+    if @duration.scan(/\d+/).length > 1
+      time = @duration.scan(/\d+/).join(".").to_f
+    else
+      time = @duration.scan(/\d+/).first.to_f
+    end
+    ((time * 3600) + Time.now.to_i).to_i
+  end
+
+  def future_forecast
+    TripForecast.new(darksky_services.get_future_forecast[:currently], @duration)
   end
 
   def trip_details
@@ -16,5 +30,9 @@ class RoadTrip
 
   def directions_services
     @_directions_services ||= DirectionsService.new(@origin, @destination)
+  end
+
+  def darksky_services
+    @_darksky_services ||= DarkskyTimeMachineService.new(@destination_lat, @destination_lng, @future_time)
   end
 end
