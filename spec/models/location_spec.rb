c464b948c53e0e55fca755f80b3603a167032619
Jori Peterson <joripeterson@gmail.com>
2019-09-16 21:55:02 -0600

    Update model testing for location

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe Location do
   it "exists" do
-    attrs = {
-      city: "Denver",
-    }
+    info =  [{:address_components=>
+            [{:long_name=>"Denver", :short_name=>"Denver", :types=>["locality", "political"]},
+            {:long_name=>"Denver County", :short_name=>"Denver County", :types=>["administrative_area_level_2", "political"]},
+            {:long_name=>"Colorado", :short_name=>"CO", :types=>["administrative_area_level_1", "political"]},
+            {:long_name=>"United States", :short_name=>"US", :types=>["country", "political"]}],
+            :geometry=>
+            {:bounds=>{:northeast=>{:lat=>39.91424689999999, :lng=>-104.6002959}, :southwest=>{:lat=>39.614431, :lng=>-105.109927}},
+              :location=>{:lat=>39.7392358, :lng=>-104.990251}}}]
 
-    location = Location.new(attrs)
+    location = Location.new(info)
 
     expect(location).to be_a Location
     expect(location.city).to eq("Denver")
