64d09f0aff794047a449817fcf194fd3113279fa
Jori Peterson <joripeterson@gmail.com>
2019-09-17 14:02:14 -0600

    Create directions service

---
+++ app/services/directions_service.rb
@@ -0,0 +1,27 @@
+class DirectionsService
+
+    def initialize(origin, destination)
+      @origin = origin
+      @destination = destination
+    end
+
+    def get_trip_details
+      get_json("/maps/api/directions/json?")
+    end
+
+    private
+
+    def conn
+      Faraday.new('https://maps.googleapis.com') do |f|
+        f.params['origin'] = "#{@origin}"
+        f.params['destination'] = "#{@destination}"
+        f.params['key'] = ENV["GEOCODE_API_KEY"]
+        f.adapter Faraday.default_adapter
+      end
+    end
+
+    def get_json(url)
+      response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
